5c797078acc35c40313b4c111dea1defa9a92b90
Julia Łągiewczyk <julia@MacBook-Air-Julia.local>
2025-02-09 09:09:55 +0100

    Wprowadzenie nowego folderu apki

---
+++ DotNetMeritoWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotNetMeritoWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
